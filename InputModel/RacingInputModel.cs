798797a492e7b63da60ce57ec3a62988a826fd82
Diego <lima.j.diego@gmail.com>
2021-06-25 14:41:16 -0300

    Criado input model

---
+++ InputModel/RacingInputModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatasCorridas.InputModel
+{
+    public class RacingInputModel
+    {
+        internal double Hour;
+
+        [Required]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "O nome do País da corrida deve conter entre 3 e 100 caracteres")]
+        public string Country { get; set; }        
+        [Required]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "O nome do Cidade da corrida deve conter entre 3 e 100 caracteres")]
+        public string City { get; set; }
+        [Required]
+        [Range(1, 1000, ErrorMessage = "Data Inválida")]
+        public  int Date { get; set; }
+
+    }
+}
