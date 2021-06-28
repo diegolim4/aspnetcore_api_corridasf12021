87906d4bca9bab9716fc934565433e46544c990a
Diego <lima.j.diego@gmail.com>
2021-06-28 07:18:46 -0300

    Criado e implementando as entidades

---
+++ Entities/Race.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatasCorridas.Entities
+{
+    public class Race
+    {
+        public Guid Id { get; set; }
+        public string Country { get; set; }
+
+        public string City { get; set; }
+
+        public int Date { get; set; }
+
+        public double Hour { get; set; }
+    }
+}
