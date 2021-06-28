b1583b0a7c311da8eb56122a2bc7bdf560d4243d
Diego <lima.j.diego@gmail.com>
2021-06-28 07:19:29 -0300

    Adicionando os exceptions

---
+++ Exceptions/RaceNotRegisteredException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatasCorridas.Exceptions
+{
+    public class RaceNotRegisteredException : FormatException
+    {
+        public RaceNotRegisteredException()
+            : base("Esta corrida não está cadastrado") { }
+    }
+}
---
+++ Exceptions/RaceRegisteredException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatasCorridas.Exception
+{
+    public class RaceRegisteredException : FormatException
+    {
+        public RaceRegisteredException()
+            : base("Está corrida já está cadastrada") { }
+    }
+}
