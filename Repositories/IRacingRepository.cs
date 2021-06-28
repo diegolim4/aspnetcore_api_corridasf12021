e4d3d5c9be0704f8785c4d52191ca7368613b221
Diego <lima.j.diego@gmail.com>
2021-06-28 07:17:44 -0300

    Adicionado o repositorio com os métodos das corridas

---
+++ Repositories/IRacingRepository.cs
@@ -0,0 +1,21 @@
+﻿using DatasCorridas.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatasCorridas.Repositories
+{
+    public interface IRacingRepository : IDisposable
+    {
+        Task<List<Race>> Catch(int pagina, int quatidade);
+        Task<Race> Catch(Guid id);
+        Task<List<Race>> Catch(string country, string city );
+        Task Insert(Race race);
+        Task Update(Race race);
+        Task Delete(Guid id);
+
+
+
+    }
+}
